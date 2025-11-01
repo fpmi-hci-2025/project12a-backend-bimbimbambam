d301ae5f27e17d5dd2891e047e53edc1ac8f8e99
Vasyalama <sanpinchy@gmail.com>
2025-11-01 05:41:30 +0300

    cors

--- src/main/java/com/bimbam/demo/config/WebConfig.java
+++ src/main/java/com/bimbam/demo/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("*") 
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
     }
