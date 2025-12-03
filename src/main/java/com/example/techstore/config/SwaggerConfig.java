7ebe3dd5674c198ba55a3db22b0ca979129c93f8
Vladimir Stasevich <vladimirss042@gmail.com>
2025-12-03 12:41:48 +0300

    change server URL to relative path

--- src/main/java/com/example/techstore/config/SwaggerConfig.java
+++ src/main/java/com/example/techstore/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .servers(List.of(new Server().url("http://localhost:8080")))
+                .servers(List.of(new Server().url("/")))
                 .info(new Info()
                         .title("Tech Store")
                         .version("1.0")
